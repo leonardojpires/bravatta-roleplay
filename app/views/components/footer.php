8e0e28dcfe3df1b451973250bfbe94fd7003147e
Leonardo Pires <165698194+leonardojpires@users.noreply.github.com>
2025-07-17 18:17:18 +0100

    Finishing landing page design

---
+++ app/views/components/footer.php
@@ -0,0 +1,14 @@
+<footer class="footer-section py-5">
+    <div class="w-full max-w-[1200px] mx-auto">
+        <div class="flex flex-col justify-center items-center gap-2 md:flex-row md:gap-5">
+            <a href="/home">Início</a>
+            <a href="/sobre">Sobre Nós</a>
+            <a href="/regras">Regras</a>
+            <a href="/noticias">Notícias</a>
+            <a href="/contacto">Contacta-nos</a>
+        </div>
+        <div class="flex flex-row justify-center items-center mt-4">
+            <p class="font-body text-center">&copy; <?php echo date("Y"); ?> Bravatta Roleplay. Todos os direitos reservados.</p> 
+        </div>
+    </div>
+</footer>
